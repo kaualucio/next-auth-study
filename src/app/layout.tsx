61a7edddfe8d6497fe8cfcf25fc12fb85b20241e
Kauã Lúcio <kauadefreitas.s992@gmail.com>
2023-06-19 14:04:00 -0300

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
-import './globals.css'
 import { Inter } from 'next/font/google'
+
+import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,8 +15,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className="h-full bg-white">
+      <body className={`${inter.className} h-full`}>{children}</body>
     </html>
   )
 }
